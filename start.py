8315a2a016c661241013559f78fd898b6beb20ce
mccoysc <364589886@qq.com>
2020-01-09 12:07:30 +0800

    Update start.py

--- start.py
+++ start.py
@@ -2,10 +2,14 @@ from twisted.web import proxy, http
 from twisted.internet import reactor
 from twisted.python import log
 import sys
+import os
+
 log.startLogging(sys.stdout)
  
 class ProxyFactory(http.HTTPFactory):
     protocol = proxy.Proxy
- 
-reactor.listenTCP(8080, ProxyFactory())
+port=8080
+if os.environ['PORT']:
+ port=os.environ['PORT']
+reactor.listenTCP(port, ProxyFactory())
 reactor.run()
